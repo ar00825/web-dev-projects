7f70bc72ca4547813f4e59a625519c137db86a36
Arian Gill <ariangill208@yahoo.com>
2026-05-12 14:57:33 +0100

    Update script.js

--- quiz-game/script.js
+++ quiz-game/script.js
@@ -1,7 +1,7 @@
 // DOM elements
 const startScreen = document.getElementById("start-screen");
 const quizScreen = document.getElementById("quiz-screen");
-const resultScreen = document.getElementById("result-screen");
+const resultScreen = document.getElementById("results-screen");
 const startButton = document.getElementById("start-btn");
 const questionText = document.getElementById("question-text");
 const answersContainer = document.getElementById("answers-container");
@@ -77,14 +77,113 @@ startButton.addEventListener("click", startQuiz);
 restartButton.addEventListener("click", restartQuiz);
 
 function startQuiz() {
-    // Reset variables
-    currentQuestionIndex = 0;
-    scoreSpan.textContent = 0;
+  // Reset variables
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreSpan.textContent = 0;
 
-    startScreen.classList.remove("active");
-    quizScreen.classList.add("active");
+  startScreen.classList.remove("active");
+  quizScreen.classList.add("active");
+
+  showQuestion();
+}
+
+function showQuestion() {
+  // reset state
+
+  answersDisabled = false;
+
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+
+  currentQuestionSpan.textContent = currentQuestionIndex + 1;
+
+  const progressPercent = (currentQuestionIndex / quizQuestions.length) * 100;
+  
+  progressBar.style.width = progressPercent + "%";
+
+  questionText.textContent = currentQuestion.question;
+
+  // removes the previous questions' answers
+  answersContainer.innerHTML = "";
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.textContent = answer.text;
+    button.classList.add("answer-btn");
+
+    button.dataset.correct = answer.correct;
+
+    button.addEventListener("click", selectAnswer);
+
+    answersContainer.appendChild(button);
+  });
+
+}
+
+function selectAnswer(event) {
+  if (answersDisabled) return
+
+  answersDisabled = true;
+
+  const selectedButton = event.target;
+  const isCorrect = selectedButton.dataset.correct === "true";
+
+  // converted into an array so that we can use the forEach method on it
+  Array.from(answersContainer.children).forEach(button => {
+    if(button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    else if (button === selectedButton) {
+      button.classList.add("incorrect");
+    }
+  });
+
+  if (isCorrect) {
+    score++;
+    scoreSpan.textContent = score;
+  }
+
+  setTimeout(() => {
+    currentQuestionIndex++;
+
+    // check if there are more questions left or the quiz has finished
+    if(currentQuestionIndex < quizQuestions.length) {
+      showQuestion()
+    }
+    else {
+      showResults()
+    }
+  }, 1000);
+}
+
+function showResults() {
+  quizScreen.classList.remove("active");
+  resultScreen.classList.add("active");
+
+  finalScoreSpan.textContent = score;
+
+  const percentage = (score / quizQuestions.length) * 100;
+
+  if(percentage === 100) {
+    resultMessage.textContent = "Extraordinary! You're a genius!";
+  }
+  else if (percentage >= 80) {
+    resultMessage.textContent = "Great Job! You know your stuff";
+  }
+  else if (percentage >= 60) {
+    resultMessage.textContent = "Good effort! Keep learning";
+  }
+  else if (percentage >= 40) {
+    resultMessage.textContent = "Not bad! Try again to improve";
+  }
+  else {
+    resultMessage.textContent = "Don't give up! You'll get better";
+  }
 }
 
 function restartQuiz() {
-    console.log("quiz restarted");
+  resultScreen.classList.remove("active");
+  
+  startQuiz();
 }
+
